5b013bb9bdb331b35b387bb1d5cd3f441714322b
neopheus <neopheus@gmail.com>
2017-02-14 10:15:13 +0100

    Initial Commit

---
+++ src/creaScrollPagination.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Neo on 25/01/2017.
+ */
+(function( $ ) {
+  "use strict";
+
+  $.fn.creaScrollPagination = function(options)
+  {
+    return this.each(function()
+    {
+      $.fn.creaScrollPagination.init($(this), options);
+    });
+  };
+  $.fn.creaScrollPagination.defaults =
+  {
+    'offsetBottom': 0,
+    'nextSelector': 'a.next:last'
+  };
+  $.fn.creaScrollPagination.init = function($e, options){
+    var _options = $.extend({}, $.fn.creaScrollPagination.defaults, options ),
+      _$window = $(window),
+      _lastUrl = null,
+      _newUrl = null,
+      _loading = false;
+
+    _$window.scroll(function(){
+      if ($(document).height() - _$window.height() - _options.offsetBottom <= _$window.scrollTop()) {
+        if (!_loading){
+          _loading = true;
+          _newUrl = $(_options.nextSelector).attr('href');
+          if (_newUrl != _lastUrl){
+            _lastUrl = _newUrl;
+            $.ajax({
+              url: _newUrl,
+              dataType: 'html',
+              success: function(html) {
+                var mydata = $(html).find('#properties');
+                $e.append(mydata);
+                _loading = false;
+                $('.pagination').hide();
+              }
+            });
+          }
+        }
+      }
+    });
+  };
+})(jQuery);
