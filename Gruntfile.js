4a14b990d56d12ef782aedffc2d68bebdabdd91e
neopheus <neopheus@gmail.com>
2017-02-15 15:40:31 +0100

    Update Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,5 +17,5 @@ module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   //register default task
-  grunt.registerTask('default', 'watch');
+  grunt.registerTask('default', 'uglify');
 };
